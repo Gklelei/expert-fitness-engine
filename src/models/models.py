ca53a15a2d8d28430694c116b260fad38083a044
gklelei <leleigideon97@gmail.com>
2026-04-14 18:28:01 +0300

    chore:  created database models

---
+++ src/models/models.py
@@ -0,0 +1,29 @@
+from src import db
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    equipment_needed = db.Column(db.Boolean, nullable=False)
+
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text, nullable=False)
+
+
+class WorkoutExercises(db.Model):
+    __tablename__ = "workouts_exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable=False)
+    reps = db.Column(db.Integer, nullable=False)
+    sets = db.Column(db.Integer, nullable=False)
+    duration_seconds = db.Column(db.Integer, nullable=False)
